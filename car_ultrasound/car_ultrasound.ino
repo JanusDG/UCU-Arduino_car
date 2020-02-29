549cda51a9115341a0d8dbaab0ddb048967ce893
Andriy Sultanov <as.andreysultanov@gmail.com>
2020-02-29 19:51:59 +0200

    ULTRASONIC DETECTOR WORKING!!!!!!!

--- car_ultrasound/car_ultrasound.ino
+++ car_ultrasound/car_ultrasound.ino
@@ -1,16 +1,6 @@
 const byte MOTOR_A = 3;  // Motor 2 Interrupt Pin - INT 3 - Right Motor
 const byte MOTOR_B = 2;  // Motor 1 Interrupt Pin - INT 2 - Left Motor
 
-// Constant for steps in disk ??????????????????????????????????????????????
-const float stepcount = 20.00;  // 20 Slots in disk, change if different
-
-// Constant for wheel diameter
-const float wheeldiameter = 25.0; // Wheel diameter in millimeters, change if different
-
-// Integers for pulse counters
-volatile int counter_A = 0;
-volatile int counter_B = 0;
-
 // Motor A
 int enA = 10;
 int in1 = 9;
@@ -26,61 +16,56 @@ int trigPin = 3;
 int echoPin = 2;
 
 // Prototypes
-void ISR_countA();
-void ISR_countB(); 
-int CMtoSteps(float cm);
-void forwards(int steps, int mspeed);
-void backwards(int steps, int mspeed);
-void right(int steps, int mspeed);
-void left(int steps, int mspeed);
+void forwards();
+void backwards();
+void right();
+void left();
  
+// Ultrasound variables
+long duration;
+int distance;
 
 void setup() 
 {
-    attachInterrupt(digitalPinToInterrupt (MOTOR_A), ISR_countA, RISING);  // Increase counter A when speed sensor pin goes High
-    attachInterrupt(digitalPinToInterrupt (MOTOR_B), ISR_countB, RISING);  // Increase counter B when speed sensor pin goes High
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+
+    analogWrite(enA, 45);
+    analogWrite(enB, 45);
 }
 
 
 void loop()
-{
+{ 
+    // Clear the trigPin by setting it to LOW:
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(5);
+  
+    // Trigger the sensor by setting the trigPin high for 10 microseconds:
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+  
+    // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
+    duration = pulseIn(echoPin, HIGH);
+    // Calculate the distance:
+    distance = (duration)/58;
     
+    if (distance < 15) {
+      forwards(); 
+      delay(200);
+    }
+    else {
+      backwards();
+      delay(200);
+    }
+
+    delay(100);
 }
-
-
-//####################################################################################################
-// Interrupt Service Routines
-// Motor A pulse count ISR
-void ISR_countA()  
-{
-    counter_A++;  // increment Motor A counter value
-} 
- 
-// Motor B pulse count ISR
-void ISR_countB()  
-{
-    counter_B++;  // increment Motor B counter value
-}
- 
-// Function to convert from centimeters to steps
-int CMtoSteps(float cm) {
-
-    int result;  // Final calculation result
-    float circumference = (wheeldiameter * 3.14) / 10; // Calculate wheel circumference in cm
-    float cm_step = circumference / stepcount;  // CM per Step
-    
-    float f_result = cm / cm_step;  // Calculate result as a float
-    result = (int) f_result; // Convert to an integer (note this is NOT rounded)
-    
-    return result;  // End and return result
- }
  
 //####################################################################################################
-void forwards(int steps, int mspeed) 
+void forwards() 
 {
-    counter_A = 0;  //  reset counter A to zero
-    counter_B = 0;  //  reset counter B to zero
-    
     // Set Motor A forward
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
@@ -88,34 +73,11 @@ void forwards(int steps, int mspeed)
     // Set Motor B forward
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
-     
-    while (steps > counter_A && steps > counter_B) {
-        if (steps > counter_A) {
-            analogWrite(enA, mspeed);
-        } 
-        else {
-            analogWrite(enA, 0);
-        }
-        if (steps > counter_B) {
-            analogWrite(enB, mspeed);
-        } else {
-            analogWrite(enB, 0);
-        }
-    }
-    
-    // Stop when done
-    analogWrite(enA, 0);
-    analogWrite(enB, 0);
-    counter_A = 0;  //  reset counter A to zero
-    counter_B = 0;  //  reset counter B to zero 
 }
  
 
-void backwards(int steps, int mspeed) 
-{
-    counter_A = 0;  //  reset counter A to zero
-    counter_B = 0;  //  reset counter B to zero
-   
+void backwards() 
+{  
     // Set Motor A reverse
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
@@ -123,33 +85,11 @@ void backwards(int steps, int mspeed)
     // Set Motor B reverse
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
-     
-    while (steps > counter_A && steps > counter_B) {
-        if (steps > counter_A) {
-            analogWrite(enA, mspeed);
-        } else {
-            analogWrite(enA, 0);
-        }
-        if (steps > counter_B) {
-            analogWrite(enB, mspeed);
-        } else {
-            analogWrite(enB, 0);
-        }
-    }
-      
-    // Stop when done
-    analogWrite(enA, 0);
-    analogWrite(enB, 0);
-    counter_A = 0;  //  reset counter A to zero
-    counter_B = 0;  //  reset counter B to zero 
 }
  
 
-void right(int steps, int mspeed) 
-{
-    counter_A = 0;  //  reset counter A to zero
-    counter_B = 0;  //  reset counter B to zero
-    
+void right() 
+{   
     // Set Motor A reverse
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
@@ -157,33 +97,11 @@ void right(int steps, int mspeed)
     // Set Motor B forward
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
-     
-    while (steps > counter_A && steps > counter_B) {
-        if (steps > counter_A) {
-            analogWrite(enA, mspeed);
-        } else {
-            analogWrite(enA, 0);
-        }
-        if (steps > counter_B) {
-            analogWrite(enB, mspeed);
-        } else {
-            analogWrite(enB, 0);
-        }
-    }
-    
-    // Stop when done
-    analogWrite(enA, 0);
-    analogWrite(enB, 0);
-    counter_A = 0;  //  reset counter A to zero
-    counter_B = 0;  //  reset counter B to zero 
 }
 
 
-void left(int steps, int mspeed) 
+void left() 
 {
-    counter_A = 0;  //  reset counter A to zero
-    counter_B = 0;  //  reset counter B to zero
-    
     // Set Motor A forward
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
@@ -191,23 +109,15 @@ void left(int steps, int mspeed)
     // Set Motor B reverse
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
-     
-    while (steps > counter_A && steps > counter_B) {
-        if (steps > counter_A) {
-            analogWrite(enA, mspeed);
-        } else {
-            analogWrite(enA, 0);
-        }
-        if (steps > counter_B) {
-            analogWrite(enB, mspeed);
-        } else {
-            analogWrite(enB, 0);
-        }
-    }
-      
-    // Stop when done
-    analogWrite(enA, 0);
-    analogWrite(enB, 0);
-    counter_A = 0;  //  reset counter A to zero
-    counter_B = 0;  //  reset counter B to zero 
+}
+
+void stop()
+{
+    // Set Motor A forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    
+    // Set Motor B reverse
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
 }
